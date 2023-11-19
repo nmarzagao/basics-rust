dccbcd319e86a323e983999432b2003fa39549b0
nmarzagao <nicolas.marzagao@gmail.com>
2023-11-19 13:03:52 -0300

    code organization

--- todo_list/target/debug/deps/todo_list-2ed2a3c743d5610e.d
+++
@@ -1,5 +0,0 @@
-/home/nicolas/Code/basics-rust/todo_list/target/debug/deps/todo_list-2ed2a3c743d5610e: src/main.rs
-
-/home/nicolas/Code/basics-rust/todo_list/target/debug/deps/todo_list-2ed2a3c743d5610e.d: src/main.rs
-
-src/main.rs:
